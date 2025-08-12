bca481b24dce99a23a6eb766d6275234fee985a5
Ghostified <bransonallan@gmail.com>
2025-08-12 23:55:16 +0300

    data pre-processing - extraction of more info from date

--- detector.py
+++ detector.py
@@ -8,11 +8,25 @@ import matplotlib.pyplot as plt
 
 #%% display the first rows of the data
 data = pd.read_csv('Data/credit_card_fraud.csv')
+print("First five rows")
 print(data.head())
 
+print("\nData shape:", data.shape)
+print("\nColumns", data.columns.to_list())
+
+#%% Convert to date time
+data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])
+data['dob'] = pd.to_datetime(data['dob'])
+
+#%% Extract useful features from dates
+data['transaction_hour'] = data['trans_date_trans_time'].dt.hour
+data['transaction_day'] = data['trans_date_trans_time'].dt.day_of_week #0=Monday
+data['customer_age'] = (data['trans_date_trans_time'] - data['dob']).dt.days //365
+
 # %% Data pre processing
-#handle missing values by filling with missing values for each column
+#handle missing values by filling with missing values for each column - only fill numeric columns 
 data.fillna(data.median(),inplace=True)
+
 
 #convert categorical columns to numerical values using one hot encoding
 data = pd.get_dummies(data, drop_first=True)
@@ -21,7 +35,7 @@ data = pd.get_dummies(data, drop_first=True)
 data['normalized_amount'] = (data['Amount'] - data['Amount'].mean()) / data['Amount'].std()
 
 #separate features and target variables
-x =  data.drop(columns=['Class'])
+X =  data.drop(columns=['Class'])
 y = data['Class']
 
 #split data into training and testing tests (80% train 20% test)
